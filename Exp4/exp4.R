031dde7ced47722287bcf064e0c2e2a55153a544
bharaths44 <bharathsatheesan@gmail.com>
2024-02-07 21:47:13 +0530

    Add na.omit() to remove missing values in titanic dataset

--- Exp4/exp4.R
+++ Exp4/exp4.R
@@ -1,11 +1,11 @@
 library(ggplot2)
 titanic <- read.csv("/Users/bharaths/Downloads/S5-ML-Lab/Exp4/titanic.csv")
+titanic <- na.omit(titanic)
 histogram <- ggplot(titanic, aes(x = Age)) +
   geom_histogram(binwidth = 5, fill = "Black", color = "White") +
   labs(
     title = "Histogram of Age information",
     x = "Age",
     y = "Frequency",
-    fill = "Survived"
   )
 print(histogram)
