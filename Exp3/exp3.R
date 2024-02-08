450981f6f440b274c7d01e6ee9b8fe5c9bd3862d
bharaths44 <bharathsatheesan@gmail.com>
2024-02-08 21:49:01 +0530

    Refactor scatter plot code in exp3.R

--- Exp3/exp3.R
+++ Exp3/exp3.R
@@ -6,17 +6,19 @@ data(iris)
 
 scatter_plot <- ggplot(iris, aes(
   x = Petal.Length,
-  y = Petal.Width, color = factor(Species), shape = factor(Species)
+  y = Petal.Width,
+  color = factor(Species),
+  shape = factor(Species)
 )) +
+  # geom_smooth(method = "glm", se = FALSE, color = "black", linetype = "dashed") +
   geom_point() +
   labs(
     title = "Scatter Plot of Petal Length VS. Petal Width",
-    x = "Petal Length", y = "Petal Width"
+    x = "Petal Length",
+    y = "Petal Width"
   ) +
   guides(
     color = guide_legend(title = "Species"),
     shape = guide_legend(title = "Species")
   )
-
-
 print(scatter_plot)
